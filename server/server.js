0da3dea00af5719675c76d437933db283cd3184c
Yurii Karmazin <karmazin.yura@gmail.com>
2020-06-03 19:30:22 +0300

    - Added path join

--- server/server.js
+++ server/server.js
@@ -19,6 +19,12 @@ const app = express();
 app.use(cors());
 
 app.use("/api/pizzas", pizzasRoute);
+
+app.use(express.static(path.join(__dirname, '/../client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../client/build/index.html`));
+});
+
 app.listen(config.PORT, () => {
   console.log("server started at " + config.PORT)
 })
